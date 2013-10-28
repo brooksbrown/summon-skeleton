484db74c918e3fdfaa217d392d3c229b14194f3a
Brooks Brown <brooks.david.brown@gmail.com>
2013-10-27 20:10:13 -0400

    initial commit

---
+++ summon-skel.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+
+
+
+if __name__ == '__main__':
+	skel_name = sys.argv[1]
+	
+
+	if skel_name == 'drupal-module':
+		from skeletons import drupalmodule as dm
+		dm.create()
+
+
+
+
+
+
+
+
+
+
+
+
